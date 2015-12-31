84770095265d9212de6867cab284dfbaf8876719
Jatin Rungta <jatinrungta27@gmail.com>
2016-01-01 00:25:41 +0530

    Codeforces 330B

---
+++ cf/330B.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define gc getchar
+#define sci scanint
+#define scli scanlong
+#define FL(i,a,b) for(int i=a;i<b;++i)
+#define MOD 1e9+7
+#define lli long long int
+#define pb push_back
+#define nline printf("\n");
+
+void sci(int &x)
+{
+    register int c = gc();
+    x = 0;
+    int neg = 0;
+    for(;((c<48 || c>57) && c != '-');c = gc());
+    if(c=='-') {neg=1;c=gc();}
+    for(;c>47 && c<58;c = gc()) {x = (x<<1) + (x<<3) + c - 48;}
+    if(neg) x=-x;
+}
+void scli(lli &x)
+{
+    register lli c = gc();
+    x = 0;
+    int neg = 0;
+    for(;((c<48 || c>57) && c != '-');c = gc());
+    if(c=='-') {neg=1;c=gc();}
+    for(;c>47 && c<58;c = gc()) {x = (x<<1) + (x<<3) + c - 48;}
+    if(neg) x=-x;
+}
+
+int main()
+{
+	int t=1;
+	//sci(t);
+	while(t--)
+	{
+		int n,m,c;
+		sci(n);
+		sci(m);
+		int a[n+1]={0};
+		FL(i,0,m)
+		{
+			sci(c);
+			a[c]=1;
+			sci(c);
+			a[c]=1;
+		}
+		FL(i,1,n+1)
+			if(a[i]==0){ c=i; break;}
+		printf("%d\n",n-1 );
+		FL(i,1,n+1)
+		{
+			if(i!=c)
+			printf("%d %d\n",i,c);
+		}
+	}
+	return 0;
+}
