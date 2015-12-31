eb283f3837b3c70abf7c7411aeeb18cd85483fc3
Jatin Rungta <jatinrungta27@gmail.com>
2015-12-31 21:32:22 +0530

    Codeforces 34B

---
+++ cf/34B.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define gc getchar
+#define sci scanint
+#define scli scanlong
+#define FL(i,a,b) for(int i=a;i<b;++i)
+#define MOD 1e9+7
+#define lli long long int
+#define pb push_back
+#define nline printf("\n");
+
+void sci(int &x)
+{
+    register int c = gc();
+    x = 0;
+    int neg = 0;
+    for(;((c<48 || c>57) && c != '-');c = gc());
+    if(c=='-') {neg=1;c=gc();}
+    for(;c>47 && c<58;c = gc()) {x = (x<<1) + (x<<3) + c - 48;}
+    if(neg) x=-x;
+}
+void scli(lli &x)
+{
+    register lli c = gc();
+    x = 0;
+    int neg = 0;
+    for(;((c<48 || c>57) && c != '-');c = gc());
+    if(c=='-') {neg=1;c=gc();}
+    for(;c>47 && c<58;c = gc()) {x = (x<<1) + (x<<3) + c - 48;}
+    if(neg) x=-x;
+}
+
+int main()
+{
+	int t=1;
+	//sci(t);
+	while(t--)
+	{
+		int n,m;
+		sci(n);
+		sci(m);
+		int a[n],t=0;
+		FL(i,0,n)
+		 sci(a[i]);
+		sort(a,a+n);
+		FL(i,0,m)
+			if(a[i]>=0)
+				break;
+			else
+				t+=a[i];
+		printf("%d",-t);
+	}
+	return 0;
+}
