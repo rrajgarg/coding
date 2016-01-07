112d93778dd87acd0e05c8e78d347e51681b9ec5
Jatin Rungta <jatinrungta27@gmail.com>
2016-01-07 23:16:07 +0530

    Codeforces 514B.cpp

---
+++ cf/514B.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define gc getchar
+#define sci scanint
+#define scli scanlong
+#define FL(i,a,b) for(int i=a;i<b;++i)
+#define MOD 1000000007
+#define lli long long int
+#define pb push_back
+#define nline printf("\n");
+
+void sci(int &x)
+{
+    register int c = gc();
+    x = 0;
+    int neg = 0;
+    for(;((c<48 || c>57) && c != '-');c = gc());
+    if(c=='-') {neg=1;c=gc();}
+    for(;c>47 && c<58;c = gc()) {x = (x<<1) + (x<<3) + c - 48;}
+    if(neg) x=-x;
+}
+void scli(lli &x)
+{
+    register lli c = gc();
+    x = 0;
+    int neg = 0;
+    for(;((c<48 || c>57) && c != '-');c = gc());
+    if(c=='-') {neg=1;c=gc();}
+    for(;c>47 && c<58;c = gc()) {x = (x<<1) + (x<<3) + c - 48;}
+    if(neg) x=-x;
+}
+
+int main()
+{
+	int t=1;
+	//sci(t);
+	while(t--)
+	{
+        int n, x,y,a,b;
+        sci(n);
+        sci(x);
+        sci(y);
+        set<double> s;
+        
+        FL(i,0,n)
+        {
+            sci(a);
+            sci(b);
+            if(a==x) s.insert(100000);
+            else s.insert((double)(b-y)/(a-x));
+        }
+        printf("%d\n",s.size() );
+	}
+	return 0;
+}
