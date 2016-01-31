72c3462d9c136f051d09e54dea45914ea614fc0e
Jatin Rungta <jatinrungta27@gmail.com>
2016-02-01 01:13:58 +0530

    Codeforces 479C

---
+++ cf/C/479C.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define gc getchar
+#define sci scanint
+#define scli scanlong
+#define FL(i,a,b) for(int i=a;i<b;++i)
+#define FLI(i,a,b) for(int i=a;i>=b;--i)
+#define MOD 1000000007
+#define lli long long int
+#define pb push_back
+#define nline printf("\n");
+
+void sci(int &x)
+{
+    register int c = gc();
+    x = 0;
+    int neg = 0;
+    for(;((c<48 || c>57) && c != '-');c = gc());
+    if(c=='-') {neg=1;c=gc();}
+    for(;c>47 && c<58;c = gc()) {x = (x<<1) + (x<<3) + c - 48;}
+    if(neg) x=-x;
+}
+void scli(lli &x)
+{
+    register lli c = gc();
+    x = 0;
+    int neg = 0;
+    for(;((c<48 || c>57) && c != '-');c = gc());
+    if(c=='-') {neg=1;c=gc();}
+    for(;c>47 && c<58;c = gc()) {x = (x<<1) + (x<<3) + c - 48;}
+    if(neg) x=-x;
+}
+
+int main()
+{
+	int t=1;
+	//sci(t);
+	while(t--)
+	{
+		int n;
+		sci(n);
+		vector<pair<int,int> > v(n);
+		FL(i,0,n)
+		{
+			int x,y;
+			sci(x);
+			sci(y);
+			v[i]=make_pair(x,y);
+		}
+		sort(v.begin(),v.end());
+		int d=0;
+		FL(i,0,n)
+		{
+			if(d<=v[i].second)
+				d=v[i].second;
+			else
+				d=v[i].first;
+		} 
+		cout<<d;
+	}
+	return 0;
+}
