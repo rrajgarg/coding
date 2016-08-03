4aeebcf75605e37cf007327d832441298c5615e0
urdarinda <jatinrungta27@gmail.com>
2016-08-03 20:42:41 +0530

    Codeforces codes

---
+++ cf/A/258A.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define gc getchar
+#define sci scanint
+#define scli scanlong
+#define FL(i,a,b) for(int i=a;i<b;++i)
+#define MOD 1000000007
+#define lli long long int
+#define pb push_back
+#define nline printf("\n");
+#define mk make_pair
+
+void sci(int &x)
+{
+    register int c = gc();
+    x = 0;
+    int neg = 0;
+    for(;((c<48 || c>57) && c != '-');c = gc());
+    if(c=='-') {neg=1;c=gc();}
+    for(;c>47 && c<58;c = gc()) {x = (x<<1) + (x<<3) + c - 48;}
+    if(neg) x=-x;
+}
+void scli(lli &x)
+{
+    register lli c = gc();
+    x = 0;
+    int neg = 0;
+    for(;((c<48 || c>57) && c != '-');c = gc());
+    if(c=='-') {neg=1;c=gc();}
+    for(;c>47 && c<58;c = gc()) {x = (x<<1) + (x<<3) + c - 48;}
+    if(neg) x=-x;
+}
+
+int main()
+{
+	int t=1;
+	//sci(t);
+	while(t--)
+	{
+		
+		string s;
+		cin>>s;
+		int f=1;
+		FL(i,0,s.length())
+		{
+			if((f&&s[i]=='0')||(f&&i==s.length()-1))
+			{
+				f=0;
+			}
+			else
+				cout<<s[i];
+
+		}
+
+	}
+	return 0;
+}
---
+++ cf/C/285C.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define gc getchar
+#define sci scanint
+#define scli scanlong
+#define FL(i,a,b) for(int i=a;i<b;++i)
+#define MOD 1000000007
+#define lli long long int
+#define pb push_back
+#define nline printf("\n");
+#define mk make_pair
+
+void sci(int &x)
+{
+    register int c = gc();
+    x = 0;
+    int neg = 0;
+    for(;((c<48 || c>57) && c != '-');c = gc());
+    if(c=='-') {neg=1;c=gc();}
+    for(;c>47 && c<58;c = gc()) {x = (x<<1) + (x<<3) + c - 48;}
+    if(neg) x=-x;
+}
+void scli(lli &x)
+{
+    register lli c = gc();
+    x = 0;
+    int neg = 0;
+    for(;((c<48 || c>57) && c != '-');c = gc());
+    if(c=='-') {neg=1;c=gc();}
+    for(;c>47 && c<58;c = gc()) {x = (x<<1) + (x<<3) + c - 48;}
+    if(neg) x=-x;
+}
+
+
+int main()
+{
+	int t=1;
+	//sci(t);
+	while(t--)
+	{
+		
+		int n;
+		sci(n);
+		lli a[n+1];
+
+		FL(i,0,n) scli(a[i]); 
+		sort(a,a+n);
+		lli c=0,k=1;
+		FL(i,0,n)
+		{
+			c+=abs(a[i]-k++);
+		}
+		cout<<c;
+
+
+	}
+	return 0;
+}
