b6c81675eda25bca2457cd9da79f39ef917f3068
urdarinda <jatinrungta27@gmail.com>
2016-08-05 23:52:41 +0530

    220A Little Elephant and Problem

---
+++ cf/A/220A.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define gc getchar
+#define sci scanint
+#define scli scanlong
+#define FL(i,a,b) for(int i=a;i<b;++i)
+#define MOD 1000000007
+#define lli long long int
+#define pb push_back
+#define nline printf("\n");
+#define mk make_pair
+
+void sci(int &x)
+{
+    register int c = gc();
+    x = 0;
+    int neg = 0;
+    for(;((c<48 || c>57) && c != '-');c = gc());
+    if(c=='-') {neg=1;c=gc();}
+    for(;c>47 && c<58;c = gc()) {x = (x<<1) + (x<<3) + c - 48;}
+    if(neg) x=-x;
+}
+void scli(lli &x)
+{
+    register lli c = gc();
+    x = 0;
+    int neg = 0;
+    for(;((c<48 || c>57) && c != '-');c = gc());
+    if(c=='-') {neg=1;c=gc();}
+    for(;c>47 && c<58;c = gc()) {x = (x<<1) + (x<<3) + c - 48;}
+    if(neg) x=-x;
+}
+
+int main()
+{
+	int t=1;
+	//sci(t);
+	while(t--)
+	{
+		int n;
+		sci(n);
+		int a[n];
+		int g=0;
+		int x,y;
+		FL(i,0,n) sci(a[i]);
+		vector<int> v(a,a+n);
+		sort(v.begin(), v.end());
+		FL(i,0,n)
+		{
+			if(a[i]!=v[i]) g++;
+		}
+		cout<<(g>2?"NO":"YES");
+	}
+	return 0;
+}
