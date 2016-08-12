99fd0762698ff42459d1212f3dce84d7c22fd654
urdarinda <jatinrungta27@gmail.com>
2016-08-12 13:22:37 +0530

    505C - Mr. Kitayuta, the Treasure Hunter

--- cf/te.cpp
+++ cf/te.cpp
@@ -39,49 +39,7 @@ int main()
 	//sci(t);
 	while(t--)
 	{
-		int n;
-		sci(n);
-		int k;
-		sci(k);
-		int c[n+1]={0};
-		int a[k+1];
-
-		lli tsum=0,ans=0;
-		FL(i,1,n+1)
-		{
-			sci(c[i]);
-			tsum+=c[i];
-
-
-		}
-		int f=1,g=1;
-		FL(i,1,k+1) 
-		{
-			sci(a[i]);
-			ans+=((c[a[i]])*(tsum-c[a[i]]));
-			if(a[i]==1)
-				{ans-=(c[a[i]]*c[n]); g=0;}
-			else ans-=(c[a[i]]*c[a[i]-1]);
-			if(a[i]==n)
-			{
-				ans-=(c[a[i]]*c[1]);
-				 f=0;
-			}
-			else ans-=(c[a[i]]*c[a[i]+1]);
-		}
 		
-		FL(i,1,n)
-		{
-			ans+=(c[i]*c[i+1]);
-		}
-		if(f)
-		ans+=(c[1]*c[n]);
-		if(g)
-		ans+=(c[1]*c[n]);
-		FL(i,1,k+1)
-			FL(j,i+1,k+1)
-				ans-=(c[a[i]]*c[a[j]]);
-		cout<<ans;
 	}
 	return 0;
 }
