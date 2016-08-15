57811ebdf4381612ad3c04c0003a422944b44a03
urdarinda <jatinrungta27@gmail.com>
2016-08-15 16:27:50 +0530

    clean

---
+++ cf/496C.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define gc getchar
+#define sci scanint
+#define scli scanlong
+#define FL(i,a,b) for(int i=a;i<b;++i)
+#define MOD 1000000007
+#define lli long long int
+#define pb push_back
+#define nline printf("\n");
+#define mk make_pair
+
+void sci(int &x)
+{
+    register int c = gc();
+    x = 0;
+    int neg = 0;
+    for(;((c<48 || c>57) && c != '-');c = gc());
+    if(c=='-') {neg=1;c=gc();}
+    for(;c>47 && c<58;c = gc()) {x = (x<<1) + (x<<3) + c - 48;}
+    if(neg) x=-x;
+}
+void scli(lli &x)
+{
+    register lli c = gc();
+    x = 0;
+    int neg = 0;
+    for(;((c<48 || c>57) && c != '-');c = gc());
+    if(c=='-') {neg=1;c=gc();}
+    for(;c>47 && c<58;c = gc()) {x = (x<<1) + (x<<3) + c - 48;}
+    if(neg) x=-x;
+}
+
+int main()
+{
+	int t=1;
+	//sci(t);
+	while(t--)
+	{
+		int n,m;
+		sci(n);
+		sci(m);
+		string a[n]; int c=0;
+		FL(i,0,n) cin>>a[i];
+		FL(i,0,m)
+		{
+			FL(j,0,n-1)
+			{
+				if(a[j][i]>a[j+1][i])
+				{
+					c++;
+					break;
+				}
+			}
+		}
+		cout<<c;
+	}
+	return 0;
+}
